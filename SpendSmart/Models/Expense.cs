1ab74d90061db7970f62cc178bced12bb1af198c
justinscottjenecke <justinjenecke@gmail.com>
2024-07-18 23:01:16 +0200

    creates model class for Expenses

---
+++ SpendSmart/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart;
+
+public class Expense
+{
+    int Id {get; set;}
+    decimal Value {get; set;}
+    
+    [Required]
+    string? Description {get; set;}
+}
